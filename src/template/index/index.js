aa54c2c307ba8df554ae72ce8f8490313504af71
JohnStream <1021283712@qq.com>
2018-03-22 16:27:27 +0800

    修改网络请求部分API

--- src/template/index/index.js
+++ src/template/index/index.js
@@ -1,11 +1,1 @@
 require('../common/nav/index.js')
-var res = require('../../util/res');
-var userInfo = {
-    username :'132',
-    password :'123'
-}
-res.request({
-    url :'/user',
-    data    : userInfo,
-    method  : 'POST'
-});
--- src/template/login/login.js
+++ src/template/login/login.js
@@ -1,2 +1,36 @@
-'use strict';
 require('./login.css');
+var _user   = require('../../../Service/user-service');
+
+var page = {
+    init: function(){
+        this.bindEvent();
+    },
+    bindEvent : function(){
+        var _this = this;
+        // 登录按钮的点击
+        $('.floating-btn').click(function(){
+            _this.submit();
+        });
+        // 如果按下回车，也进行提交
+        $('.user-content').keyup(function(e){
+            // keyCode == 13 表示回车键
+            if(e.keyCode === 13){
+                _this.submit();
+            }
+        });
+    },
+    submit : function(){
+       var formdata = {
+        username : $.trim($('#username').val()),
+        password : $.trim($('#password').val())
+       }
+       _user.login(formdata,function(res){
+           console.log(res);
+    }, function(errMsg){
+        console.log(errMsg);
+    })
+    }
+}
+$(function(){
+    page.init();
+});
